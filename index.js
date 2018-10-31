26c5192373ad369a8d1ae856d8c008bcdbd36c28
Kirill Kornienko <kiril_bel@mail.ru>
2018-10-31 16:19:37 +0300

    Create .css file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const arr = [];
+
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
@@ -8,21 +8,8 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/add/:name', (req, res)=>{
-  arr.push(req.params.name);
-  res.send('OK');
-});
-
-app.get('/delete/:name', (req, res)=>{
-  let item = 0;
-  while (item<=arr.length) {
-    let y = arr.indexOf(req.params.name); //eslint-disable-line
-    if (req.params.name===arr[item]) {
-      arr.splice(y, 1);
-    } else if (y === -1) break;
-    item++;
-  }
-  res.send('OK');
+app.get('/change', (req, res)=>{
+  res.sendFile(__dirname+'/change_page.html');
 });
 
 app.listen(5000, ()=> console.log('Server has been started'));
