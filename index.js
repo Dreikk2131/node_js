8d085b123f5173e7d59256b0ecf341e82265e635
Kirill Kornienko <kiril_bel@mail.ru>
2018-11-01 12:54:56 +0300

    Folder "static" is create

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/static"));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname+'/start.html');
+  res.sendFile(__dirname+'/static/start.html');
 });
 
 
 app.get('/change', (req, res)=>{
-  res.sendFile(__dirname+'/change_page.html');
+  res.sendFile(__dirname+'/static/change_page.html');
 });
-
-app.use('/', router);
 
 app.listen(5000, ()=> console.log('Server has been started'));
