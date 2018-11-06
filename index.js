96af2f38b2bd36763a31544bf782f4552da4ccf9
Kirill Kornienko <kiril_bel@mail.ru>
2018-11-06 09:15:46 +0300

    Chanhe textarea on input

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + "/static"));
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname+'/static/start.html');
+  res.sendFile(__dirname + '/static/start.html');
 });
 
+app.get('/download', (req, res) => {
+  res.download(__dirname + '/static/img/nach1.jpg');
+});
 
-app.get('/change', (req, res)=>{
+app.get('/change', (req, res) => {
   res.sendFile(__dirname+'/static/change_page.html');
 });
 
