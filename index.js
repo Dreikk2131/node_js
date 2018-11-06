1e6cb0c1806a6e57c86984bb6cc1c22c8a344dbe
Kirill Kornienko <kiril_bel@mail.ru>
2018-11-06 15:39:08 +0300

    Use pug for build page

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/static'));
+app.set('views', './pugF');
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/pugF'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/static/start.html');
+  res.render('start');
 });
 
 app.get('/download', (req, res) => {
   res.download(__dirname + '/static/img/nach1.jpg');
 });
 
-app.get('/change', (req, res) => {
-  res.sendFile(__dirname+'/static/change_page.html');
+app.get('/change_page', (req, res) => {
+  res.render('change_page');
 });
 
 app.listen(5000, ()=> console.log('Server has been started'));
