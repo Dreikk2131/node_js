ffbf90576ee6949b80d02651d2a7fe12b85172aa
Kirill Kornienko <kiril_bel@mail.ru>
2018-11-08 12:25:10 +0300

    Rename pugF to views. Fix Dmitriy message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.set('views', './pugF');
+app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/static'));
 
